2bd1b82bc0e95848a4ee96233aae77db7fe0c632
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-10-19 21:52:27 +0100

    EXCERSISE 7
    Completed excersise 7 and added comments so you can see what I've done.

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-var user = {
-    name: "jordan8way",
-    password: "Zerotomastery",
-};
+// 1. Create an object that has properties "username" and "password". 
+// Fill those values in with strings.
+{
+	username: "Jordan8way",
+	password: "Zerotomastery"
+}
+// 2. Create an array which contains the object you have made above and name 
+// the arry "database"
+var database = [
+var database = {
+    username: "Jordan8way"
+    password: "Zerotomastery"
+}
+// 3. Create an array called "newsfeed" which contains 
+// 3 objects with properties "username" and "timeline".
+var newsFeed = [
+{
+    username: "Jordan8way"
+    timeline: "Javascript is fun!"
+},
+{
+    username: "Andrei"
+    timeline: "WHASSSSSAAAAPPPPP"
+},
+{
+    username: "Lauren"
+    timeline: "psychoneuroimmunology"
+}
+];
