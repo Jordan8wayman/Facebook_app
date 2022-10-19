a444746e8422af5c025fcbf1f8e8e327642c49d6
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-10-19 22:15:05 +0100

    SIMPLIFIED FACEBOOK
    created a very basic facebook. following along to the ztm lesson.

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-// 1. Create an object that has properties "username" and "password". 
-// Fill those values in with strings.
-{
-	username: "Jordan8way",
-	password: "Zerotomastery"
-}
-// 2. Create an array which contains the object you have made above and name 
-// the arry "database"
 var database = [
-var database = {
-    username: "Jordan8way"
+{
+    username: "Jordan8way",
     password: "Zerotomastery"
 }
-// 3. Create an array called "newsfeed" which contains 
-// 3 objects with properties "username" and "timeline".
+];
+
 var newsFeed = [
 {
-    username: "Jordan8way"
+    username: "Jordan8way",
     timeline: "Javascript is fun!"
 },
 {
-    username: "Andrei"
+    username: "Andrei",
     timeline: "WHASSSSSAAAAPPPPP"
 },
 {
-    username: "Lauren"
+    username: "Lauren",
     timeline: "psychoneuroimmunology"
 }
 ];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("what is your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username &&
+        pass === database[0].password) {
+            console.log(newsFeed);
+        }
+    else {
+        alert("Sorry username or password not recognised.")
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
